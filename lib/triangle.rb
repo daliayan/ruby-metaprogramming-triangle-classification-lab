1a0d358f67c504bc1f5b2768e7e507b8b887289b
daliayan <daliayan5@gmail.com>
2021-02-08 21:18:53 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
-  # write code here
+
+  attr_reader :triangle_a, :triangle_b, :triangle_c
+
+  def initialize(triangle_a, triangle_b, triangle_c)
+    @triangle_a = triangle_a
+    @triangle_b = triangle_b
+    @triangle_c = triangle_c
+  end
+
+  def kind
+    kind_of_triangle
+    if triangle_a == triangle_b && triangle_b == triangle_c
+      :equilateral
+    elsif
+      triangle_a == triangle_b || triangle_b == triangle_c || triangle_c == triangle_a
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def kind_of_triangle
+    triangles_combo = [(triangle_a +  triangle_b > triangle_c), (triangle_a + triangle_c > triangle_b), (triangle_b + triangle_c > triangle_a)]
+      [triangle_a, triangle_b, triangle_c].each do |side|
+        triangles_combo << false if side <= 0
+      raise TriangleError if triangles_combo.include?(false)
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
